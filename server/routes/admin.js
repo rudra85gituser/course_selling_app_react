ae5b84090cb297da433b5672ed04a847b9e651c5
Rudra Yadav <realrudrayadav@gmail.com>
2024-10-07 23:56:34 +0530

    work

---
+++ server/routes/admin.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const cors =require('cors');
+const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+const {User , Course , Admin} =require("..db");
+const  {SECRET} =require("../middleware/auth");
+const {authenticateJwt} = require("..middleware/auth");
+
+const router =express.Router();
+
+//redirecting the user to the user profile page
+router.get('/me' ,authenticateJwt , (req , res)=>{
+    res.json({
+      username: req.user.username,
+      //email: req.user.email
+    })
+});
+
+router.post('/signup', (req, res) => {
+    const { username, password } = req.body;
+    function callback(admin) {
+      if (admin) {
+        res.status(403).json({ message: 'Admin already exists' });
+      } else {
+        const obj = { username: username, password: password };
+        const newAdmin = new Admin(obj);
+        newAdmin.save();
+        const token = jwt.sign({ username, role: 'admin' }, SECRET, { expiresIn: '1h' });
+        res.json({ message: 'Admin created successfully', token });
+      }
+  
+    }
+    Admin.findOne({ username }).then(callback);
+  });
+  
+  router.post('/login', async (req, res) => {
+    const { username, password } = req.headers;
+    const admin = await Admin.findOne({ username, password });
+    if (admin) {
+      const token = jwt.sign({ username, role: 'admin' }, SECRET, { expiresIn: '1h' });
+      res.json({ message: 'Logged in successfully', token });
+    } else {
+      res.status(403).json({ message: 'Invalid username or password' });
+    }
+  });
+  
+  router.post('/courses', authenticateJwt, async (req, res) => {
+    const course = new Course(req.body);
+    await course.save();
+    res.json({ message: 'Course created successfully', courseId: course.id });
+  });
+  
+  router.put('/admin/courses/:courseId', authenticateJwt, async (req, res) => {
+    const course = await Course.findByIdAndUpdate(req.params.courseId, req.body, { new: true });
+    if (course) {
+      res.json({ message: 'Course updated successfully' });
+    } else {
+      res.status(404).json({ message: 'Course not found' });
+    }
+  });
+  
+  router.get('/courses', authenticateJwt, async (req, res) => {
+    const courses = await Course.find({});
+    res.json({ courses });
+  });
+  
+  module.exports =router;
